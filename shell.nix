2abf28b46502e4f5a8ddd289dcd2e170d23122cb
Oscar Ahlén <github@ahlen.dev>
2025-10-22 11:51:04 +0200

    Add dev shell

---
+++ shell.nix
@@ -0,0 +1,19 @@
+let
+  sources = import ./npins;
+  pkgs = import sources.nixpkgs { };
+in
+pkgs.mkShell {
+  NIX_SHELL = "Rust";
+
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+  packages = with pkgs; [
+    cargo
+    clippy
+    openssl
+    pkg-config
+    rust-analyzer
+    rustc
+    rustfmt
+  ];
+}
